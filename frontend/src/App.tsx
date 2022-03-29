aaca872798e9babba3ac73820b0b80e43d107426
J-3d <akim99999999@gmail.com>
2022-03-29 21:55:42 +0300

    Added form for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Layout from "./Layout";
 import Button from "@mui/material/Button";
 import axios from 'axios';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function App() {
-  const fetchSnake = () => {
-    axios.post("/api/snakes/mint", {
-      "snakeId": "aaaaaaaa",
-      "patternId": "pattern_001",
-      "attrId": "element_002",
-    }).then(res => {
-        window.open(res.data.data.url);
+  const [formData, setFormData] = useState({"snakeId": "", "patternId": "", "attrId": "", "schemaId": ""});
+  const [imageUrl, setImageUrl] = useState("");
+
+  const fetchSnake = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    axios.post("/api/snakes/mint", formData).then(res => {
+        setImageUrl(res.data.data.url);
     }).catch(console.log);
+  }
+
+  const formInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.getAttribute('name') as string;
+    setFormData({ ...formData, [name]: event.target.value });
   }
 
   return (
     <Layout>
-      <Button onClick={fetchSnake}>Сминтить картинку</Button>
+      <Box component="form" onSubmit={fetchSnake} sx={{mb: 2}}>
+        <TextField id="snake-id" name="snakeId" required label="Snake id" onChange={formInputHandler} variant="standard" margin="normal" />
+        <TextField id="pattern-id" name="patternId" label="Pattern id" onChange={formInputHandler} variant="standard" margin="normal" />
+        <TextField id="attr-id" name="attrId" label="Attr id" onChange={formInputHandler} variant="standard" margin="normal" />
+        <TextField id="scheme-id" name="schemaId" label="Color scheme id" onChange={formInputHandler} variant="standard" margin="normal" />
+        <Button type="submit" >Сминтить картинку</Button>
+      </Box>
+      {
+        imageUrl && (
+          <Box>
+            <img width="96" height="96" src={imageUrl} alt={"snake"}/>
+          </Box>
+        )
+      }
+
     </Layout>
   );
 }
--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function Layout({children}: Props) {
     return (
-        <Box sx={{display: "flex"}}>
+        <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
             {children}
         </Box>
     );
